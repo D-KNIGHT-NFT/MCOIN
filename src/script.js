cfe2939ffe3566dce6b877cf0b8c0a88742575a9
D-KNIGHT <d-knight@tuta.io>
2022-04-18 19:18:13 +0200

    Added utils for prelad webfonts and a 3D hover class

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import './css/style.css'
 
 // Main Libraries
 import $ from "jquery";
+import preloadFonts from "../js/utils"
 import * as THREE from 'three'
 import * as PIXI from "pixi.js"
 import { SVG, extend as SVGextend, Element as SVGElement } from '@svgdotjs/svg.js'
@@ -46,6 +47,67 @@ import SimplexNoise from "simplex-noise";
 import hsl from "hsl-to-hex";
 import debounce from "debounce";
  
+
+
+
+////////////////////////////////////////////////////////////////////
+// HOVER 3D 
+// https://github.com/PavelLaptev/Hover3D.js
+///////////////
+
+class Hover3D 
+{
+    constructor(id)
+    {
+        this.id = id;
+        this.xOffset = 10;
+        this.yOffset = 10;
+        this.attack = 0.1;
+        this.release = 0.5;
+        this.perspective = 500;
+        this.create();
+    }
+
+    create()
+    {
+        document.querySelectorAll(this.id).forEach(element => 
+        {
+            const rectTransform = element.getBoundingClientRect();
+            const perspective = "perspective(" + this.perspective + "px) ";
+            element.style.setProperty("transform-style", "preserve-3d");
+            
+            element.addEventListener("mouseenter", e =>
+            {
+                element.style.setProperty("transition", "transform " + this.attack + "s");
+            })
+
+            element.addEventListener("mousemove", e => 
+            {
+                let dy = e.clientY - rectTransform.top;
+                let dx = e.clientX - rectTransform.left;
+                let xRot = this.map(dx, 0, rectTransform.width, -this.xOffset, this.xOffset);
+                let yRot = this.map(dy, 0, rectTransform.height, this.yOffset, -this.yOffset);
+                let propXRot = "rotateX(" + yRot + "deg) ";
+                let propYRot = "rotateY(" + xRot + "deg)";
+
+                element.style.setProperty("transform", perspective + propXRot + propYRot);
+            })
+
+            element.addEventListener("mouseleave", e => 
+            {
+                element.style.setProperty("transition", "transform " + this.release + "s");
+                element.style.setProperty("transform", perspective + "rotateX(0deg) rotateY(0deg)");
+            })
+        })
+    }
+    // Processing map() function
+    map(value, istart, istop, ostart, ostop) 
+    {
+        return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+    }
+}
+
+let myHover3D = new Hover3D(".div1");
 
 
 ////////////////////////////////////////////////////////////////////
@@ -857,7 +919,7 @@ controls.autoRotate= true
 controls.enableZoom = true
 controls.autoRotateSpeed = 1
 controls.minDistance = 0.3;
-controls.maxDistance = 2.5;
+controls.maxDistance = 4.5;
 controls.target.set( 0, 0, 0 );
 
 ////////////////////////////////////////////////////////////////////
